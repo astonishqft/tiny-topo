ccec94d8ff0f3f604dfe993f2830402d29d5f3a2
qi.futao <1551601581@qq.com>
2024-03-26 09:37:32 +0800

    代码优化

--- packages/engine/src/TinyEditor.ts
+++ packages/engine/src/TinyEditor.ts
@@ -15,10 +15,6 @@ class TinyEditor {
   createShape(type: string, x: number, y: number) {
     this.painter.createShape(type, x, y)
   }
-
-  setLineType(type: string) {
-    this.painter.setLineType(type)
-  }
 }
 
 export default TinyEditor
